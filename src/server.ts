8cbfc1d0e72bb9b3a590617d79477f88be00b03c
Celso Alexandre <celsoalexandre@live.com>
2021-08-09 22:27:31 -0300

    Resolvers

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,62 @@
 import { ApolloServer, gql } from 'apollo-server';
 
 const typeDefs = gql`
+  scalar Date
+
+  type Usuario {
+    id: ID
+    nome: String!
+    email: String!
+    idade: Int
+    salario: Float
+    vip: Boolean
+  }
+
+  type Produto {
+    nome: String
+    preco: Float
+    desconto: Float
+    precoComDesconto: Float
+  }
+
   #Pontos de entrada da API
   type Query {
-    hi: String
-    whatTimeItIs: String
+    hi: String!
+    whatTimeItIs: Date!
+    usuarioLogado: Usuario
+    produtoEmDestaque: Produto
   }
 `;
 
 const resolvers = {
+  Produto: {
+    precoComDesconto(parent) {
+      return parent.preco * (1 - parent.desconto);
+    },
+  },
   Query: {
     hi() {
       return 'Whatever';
     },
     whatTimeItIs() {
-      return new Date().toString();
+      return new Date();
+    },
+    usuarioLogado() {
+      return {
+        ID: '1',
+        nome: 'Celso Alexandre',
+        email: 'celsoalexandre@live.com',
+        idade: 28,
+        salario: 100.20,
+        vip: true,
+      };
+    },
+    produtoEmDestaque() {
+      return {
+        nome: 'Uva (Kg)',
+        preco: 5,
+        desconto: 0.05,
+      };
     },
   },
 };
